2dc4a058d7e2351238185d022df73584dab78203
Ovidiu Stoica <ovidiu.stoica1094@gmail.com>
2021-11-04 11:23:52 +0200

    Fix user picture

--- src/designvote/account/routes.clj
+++ src/designvote/account/routes.clj
@@ -1,7 +1,8 @@
 (ns designvote.account.routes
   (:require [designvote.middleware :as mw]
             [designvote.account.handlers :as account]
-            [clojure.spec.alpha :as s]))
+            [clojure.spec.alpha :as s]
+            [spec-tools.data-spec :as ds]))
 
 (defn routes
   [env]
@@ -15,26 +16,26 @@
                 :summary    "Retrieve current logged in user"}
        :post   {:handler    (account/create-account! db)
                 :responses  {201 {:body any?}}
-                :parameters {:body {:email    string?
-                                    :social   boolean?
-                                    :uid      string?
-                                    :name     (s/nilable string?)
-                                    :nickname string?
-                                    :picture  string?
-                                    :provider string?
-                                    :token    string?}}
+                :parameters {:body {:email            string?
+                                    :social           boolean?
+                                    :uid              string?
+                                    :name             (s/nilable string?)
+                                    :nickname         string?
+                                    (ds/opt :picture) string?
+                                    :provider         string?
+                                    :token            string?}}
                 :summary    "Create an account"}
        :delete {:handler    (account/delete-account! db auth0)
                 :responses  {204 {:body nil?}}
                 :middleware [[mw/wrap-auth0]]
                 :summary    "Delete an account"}}]
      ["/public/:user-id"
-      {:get {:handler   (account/get-public-user db)
+      {:get {:handler    (account/get-public-user db)
              :parameters {:path {:user-id string?}}
-             :responses {200 {:body {:name    (s/nilable string?)
-                                     :picture string?
-                                     :nickname string?
-                                     :uid     string?}}}}}]
+             :responses  {200 {:body {:name     (s/nilable string?)
+                                      :picture  string?
+                                      :nickname string?
+                                      :uid      string?}}}}}]
 
      ["/uid"
       {:post {:handler    (account/create-account-from-uid! db auth0)
